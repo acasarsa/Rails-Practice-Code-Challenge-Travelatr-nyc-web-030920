020a2a77c060716ef74b65526803a0e9d4a852d9
github username <github email address>
2020-04-08 17:50:23 -0400

    A link to that blogger's featured post

--- app/models/blogger.rb
+++ app/models/blogger.rb
@@ -4,4 +4,16 @@ class Blogger < ApplicationRecord
   validates :name, uniqueness: true
   validates :age, numericality: {greater_than: 0}
   validates :bio, length: {minimum: 30}
+
+  # def most_liked_post
+  #   self.posts.each do |like|
+  #     likes
+  #   end
+  # end
+
+  def most_popular_post
+    self.posts.max_by {|post| post.likes}
+  end
 end
+
+# Blogger.second.posts.max_by(2) {|post| post.likes}
