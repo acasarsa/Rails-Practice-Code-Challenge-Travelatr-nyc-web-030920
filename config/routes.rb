6b355ed0a3d746d618c37314ba03b18f51d182e7
github username <github email address>
2020-04-08 16:35:16 -0400

    pair programming

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+  # get 'posts/new'
+
+  # get 'po
+resources :bloggers, only: [:new, :create, :show]
+resources :posts, only: [:show, :new, :create, :edit, :update]
+resources :destinations, only: :show 
+
+  # get 'posts/show'
+
+  # get 'destinations/show'
+
+  # get 'bloggers/new'
+
+  # get 'bloggers/show'
+  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
